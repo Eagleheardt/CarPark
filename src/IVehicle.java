9153e42afe9f120961c9a4e047cda1c76439d1ef
Andre Gueret <andre.gueret@ruralsource.com>
2019-02-20 08:30:17 -0600

    added basic classes

---
+++ src/IVehicle.java
@@ -0,0 +1,16 @@
+
+public interface IVehicle {
+
+    int getNumberOfWheels();
+
+    int getNumberOfSeats();
+
+    double getPrice();
+
+    String[] getFeatures();
+
+    void addFeature(String feature);
+
+    void testDrive();
+
+}
